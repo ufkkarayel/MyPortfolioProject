e95f42abdf4e84b55481f092351b154f5b9da92e
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-03 20:34:58 +0300

    listeleme işlemleri eklendi.

---
+++ MyPortfolioProject/Controllers/ServiceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyPortfolioProject.DAL.Context;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        Context db = new Context();
+        
+        public ActionResult Index()
+        {
+            var values = db.Services.ToList();
+            return View(values);
+        }
+        public ActionResult AddService()
+        {
+            return View();
+        }
+    }
+}
